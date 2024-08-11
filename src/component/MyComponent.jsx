75dc41ab4246af6fd108b47187a012e8444dd024
Shubham926 <bhandarishubham926@gmail.com>
2024-08-11 17:14:20 +0530

    second

--- src/component/MyComponent.jsx
+++ src/component/MyComponent.jsx
@@ -5,64 +5,64 @@ import { stringify } from "postcss"
 
 function MyComponent() {
     const [showModel, setShowModel] = useState(false)
-    const [taskList,setTaskList]=useState([])
+    const [taskList, setTaskList] = useState([])
 
-useEffect(()=>{
-    let arr=localStorage.getItem("taskList");
-   
-    if(arr){
-        let obj=JSON.parse(arr)
-        setTaskList(obj)
+    useEffect(() => {
+        let arr = localStorage.getItem("taskList");
+
+        if (arr) {
+            let obj = JSON.parse(arr)
+            setTaskList(obj)
+        }
+
+    }, [])
+
+
+    const deleteTask = (index) => {
+        let tempList = taskList
+        tempList.splice(index, 1)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
     }
 
-},[])
 
 
-const deleteTask=(index)=>{
-    let tempList=taskList
-    tempList.splice(index,1)
-    localStorage.setItem("taskList",JSON.stringify(tempList))
-    setTaskList(tempList)
-    window.location.reload()
-}
-
-
-
-    const saveTask=(taskObj)=>{
-let tempList=taskList
-taskList.push(taskObj)
-localStorage.setItem("taskList", JSON.stringify(tempList))
-setTaskList(tempList)
-setShowModel(false)
+    const saveTask = (taskObj) => {
+        let tempList = taskList
+        taskList.push(taskObj)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        setShowModel(false)
     }
 
-    const updateListArray=(obj,index)=>{
-        let tempList=taskList
-        tempList[index]=obj
-        localStorage.setItem("taskList",JSON.stringify(tempList))
+    const updateListArray = (obj, index) => {
+        let tempList = taskList
+        tempList[index] = obj
+        localStorage.setItem("taskList", JSON.stringify(tempList))
         setTaskList(tempList)
         window.location.reload();
     }
 
     return (
         <>
-        <div className="bg-slate-200">
-         
+            <div className="bg-slate-200">
+
                 <div className="text-center bg-slate-300 h-60">
                     <h1 className="text-4xl font-bold font-serif m-3 p-2">Todo List</h1>
                     <button onClick={() => setShowModel(true)} className="bg-blue-700 w-32 h-10 rounded-3xl font-serif text-white m-2 p-2 ">Create Task</button>
-                    
+
 
                 </div>
-                
-                
-                {showModel && <CreateTask save={saveTask} onClose={()=>setShowModel(false)} />}
-                   <div className=" p-10 m-2 w-[100%] flex flex-wrap gap-10 justify-center ">
-                    {taskList.map((obj,index)=><Card taskName = {obj.Name}  taskDesc = {obj.Description} index = {index} deleteTask={deleteTask} updateListArray={updateListArray}/>)}
-                    </div> 
-                    <Card/>
 
-                    </div>
+
+                {showModel && <CreateTask save={saveTask} onClose={() => setShowModel(false)} />}
+                <div className=" p-10 m-2 w-[100%] flex flex-wrap gap-10 justify-center ">
+                    {taskList.map((obj, index) => <Card taskName={obj.Name} taskDesc={obj.Description} index={index} deleteTask={deleteTask} updateListArray={updateListArray} />)}
+                </div>
+
+
+            </div>
 
         </>
     )
